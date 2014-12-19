65c69c5fee218c30474093cb6f0d0acff80bf202
walkor <walkor@workerman.net>
2014-12-19 23:35:02 +0800

    源代码链接

--- applications/Flappybird/Web/index.php
+++ applications/Flappybird/Web/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 <title>workerman-flappy-bird 多人在线版 </title>
 <meta name="description" content="">
 <meta name="keywords" content="flappy bird 多人在线版 workerman">
@@ -30,5 +31,7 @@ var cpro_id = "u1576789";
 var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
 document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fabcf9330bef79b4aba5b24fa373506d9' type='text/javascript'%3E%3C/script%3E"));
 </script>
+<br>
+<center style="color:333333;font-weight:bold;"><a href="http://www.workerman.net/workerman-flappybird">源代码及安装教程</a></center>
 </body>
 </html>
